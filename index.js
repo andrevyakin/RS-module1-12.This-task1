c007ec43515c4acc2e09983bb027144c7d5f0eb5
Андрей <andrevyakin@gmail.com>
2022-11-28 09:24:55 +0300

    added another way

--- index.js
+++ index.js
@@ -31,3 +31,39 @@ console.log(student);
 
 student.level = 3;
 console.log(student.improveLevel());
+
+//Или "в лоб", если есть уверенность что level никто не изменит
+const student2 = {
+    stack: ["HTML"],
+    level: 1,
+    improveLevel() {
+        this.level++;
+        switch (this.level) {
+            case 2:
+                this.stack.push("CSS");
+                break;
+            case 3:
+                this.stack.push("JavaScript");
+                break;
+            case 4:
+                this.stack.push("React");
+                break;
+            case 5:
+                this.stack.push("NodeJS");
+                break;
+            default:
+                alert("Студент выучил все технологии!");
+        }
+        return this;
+    }
+}
+
+student2
+    .improveLevel()
+    .improveLevel()
+    .improveLevel()
+    .improveLevel()
+    .improveLevel()
+
+console.log( student2);
+
